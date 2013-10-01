27efd200b86cab334ea70b9f80821945d2024649
ramon830 <ramonr830@gmail.com>
2013-10-01 18:45:31 +0300

    add class View

--- src/com/tictoe/Field.java
+++ src/com/tictoe/Field.java
@@ -2,4 +2,8 @@ package com.tictoe;
 
 
 public class Field {
+    private final int sizeField = 20;
+    private char[][] field = new char[sizeField ][sizeField ];
+
+
 }
---
+++ src/com/tictoe/View.java
@@ -0,0 +1,5 @@
+package com.tictoe;
+
+
+public class View {
+}
