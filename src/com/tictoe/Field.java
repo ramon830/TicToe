5eca589f7357bbb9b8b0fd7f789cfb2dca95d6c5
ramon830 <ramonr830@gmail.com>
2013-10-01 19:06:44 +0300

    update class Main

--- src/com/tictoe/Field.java
+++ src/com/tictoe/Field.java
@@ -3,7 +3,17 @@ package com.tictoe;
 
 public class Field {
     private final int sizeField = 20;
-    private char[][] field = new char[sizeField ][sizeField ];
+    private char[][] fieldArray = new char[sizeField ][sizeField ];
 
+    public char[][] getFieldArray() {
+        return fieldArray;
+    }
 
+    public void setFieldArray(char[][] fieldArray) {
+        this.fieldArray = fieldArray;
+    }
+
+    public int getSizeField() {
+        return sizeField;
+    }
 }
--- src/com/tictoe/View.java
+++ src/com/tictoe/View.java
@@ -2,4 +2,18 @@ package com.tictoe;
 
 
 public class View {
+    private Field field = new Field();
+
+    public View(Field field) {
+        this.field = field;
+    }
+
+    public void draw() {
+        for (int i = 0; i < field.getSizeField(); i++) {
+            for (int j = 0; j < field.getSizeField(); j++) {
+                System.out.print("[" + field.getFieldArray()[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
 }
